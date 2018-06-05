35e943db409d2fc4dba08493a3c3617e1433e8b8
David-Viuche <39018284+David-Viuche@users.noreply.github.com>
2018-06-05 10:28:55 -0500

    Update Node.java

--- Corte 2/POD_HackerEarth/monk_and_Philosophers_Stone_01/Node.java
+++ Corte 2/POD_HackerEarth/monk_and_Philosophers_Stone_01/Node.java
@@ -6,10 +6,12 @@ public class Node {
 	 * NRC: 6285
 	 */
 	
+	//valor del nodo
 	public int valor;
 
-	public Node next; // Pointer
+	public Node next; // apuntador del nodo siguiente
 
+	//constructores 
 	public Node() {}
 
 	public Node(int valor) {
@@ -19,10 +21,12 @@ public class Node {
 
 	}
 
+	//funcion que retorna el valor del nodo en forma de String
 	public String toString() {
 		return String.valueOf(valor)+" ";
 	}
 
+	//metodo que retorna un nuevo objeto con el mismo valor del nodo
 	public Node clone() {
 		Node clone = new Node(this.valor);
 		return clone;
