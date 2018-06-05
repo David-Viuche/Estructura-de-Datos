b3570da8b8a894ec7dd91514adf43ac1159ef3fe
David-Viuche <39018284+David-Viuche@users.noreply.github.com>
2018-06-05 10:37:51 -0500

    Create Node.java

---
+++ Corte 2/URI/URI_Tree_Recovery_1191/Node.java
@@ -0,0 +1,34 @@
+package URI_Tree_Recovery_1191;
+
+/*Author: David Viuche 
+ * 
+ * Nodo del Arbol binario implementado para funcionar con letras
+ * */
+
+public class Node {
+
+	//valor del nodo
+	int value;
+
+	//hijo de la derecha
+	Node right = null;
+	//hijo de la izquierda
+	Node left = null;
+
+	//constructor del nodo recibe como parametro el valor del nodo
+	public Node(int value) {
+
+		this.value = value;
+	}
+
+	//retorna un dato de tipo String con el valor del nodo
+	public String toString() {
+		return "Valor: " + this.value;
+	}
+	
+	//crea un clon del nodo y lo retorna 
+	public Node clone() {
+		Node clone = new Node(this.value);
+		return clone;
+	}
+}
