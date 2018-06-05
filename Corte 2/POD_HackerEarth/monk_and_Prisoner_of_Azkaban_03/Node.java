e915a34d8b8b1e063da2f384eb081159e0e92408
David-Viuche <39018284+David-Viuche@users.noreply.github.com>
2018-06-05 10:33:20 -0500

    Update Node.java

--- Corte 2/POD_HackerEarth/monk_and_Prisoner_of_Azkaban_03/Node.java
+++ Corte 2/POD_HackerEarth/monk_and_Prisoner_of_Azkaban_03/Node.java
@@ -2,17 +2,20 @@ package monk_and_Prisoner_of_Azkaban_03;
 
 public class Node {
 
-	/*@author David Viuche 
-	 * NRC: 6285
+	/*
+	 * @author David Viuche NRC: 6285
 	 */
-	
+
+	// valores del nodo
 	public int valor;
 	public int pos;
 
-	public Node next; // Pointer
-	public Node back;
-	
-	public Node() {}
+	public Node next; // apuntador del nodo siguiente
+	public Node back;// apuntador del nodo atras
+
+	//contructores 
+	public Node() {
+	}
 
 	public Node(int valor, int pos) {
 
@@ -21,15 +24,15 @@ public class Node {
 
 	}
 
+	//funcion que retorna un dato de tipo String con el valor del nodo 
 	public String toString() {
-		return String.valueOf(valor)+" pos: "+pos+" ";
+		return String.valueOf(valor) + " pos: " + pos + " ";
 	}
 
+	//funcion que retorna el clon del nodo
 	public Node clone() {
 		Node clone = new Node(this.valor, this.pos);
 		return clone;
 	}
-	
-	
 
 }
