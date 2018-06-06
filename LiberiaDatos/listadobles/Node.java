6ca4a374f30ae66ff905878d92eb0d6787ae9882
David-Viuche <39018284+David-Viuche@users.noreply.github.com>
2018-06-05 23:10:21 -0500

    Create Node.java

---
+++ LiberiaDatos/listadobles/Node.java
@@ -0,0 +1,37 @@
+package listadobles;
+
+public class Node {
+
+		//datos del nodo
+		public String name;
+		public int age;
+		public int id;
+
+		public Node next; // apuntador del nodo siguiente
+		public Node back; // apuntador del nodo anterior
+
+		//constructores
+		public Node() {}
+
+		public Node(String name, int age, int id) {
+
+			this.name = name;
+			this.age = age;
+			this.id = id;
+
+		}
+
+		//metodo que retorna un dato de tipo String con el valor del nodo
+		public String toString() {
+			return "Name: " + this.name + "\t Age: " + this.age + "\t ID: " + this.id + "\n";
+		}
+
+		//metodo que retorna el clon del nodo
+		public Node clone() {
+			Node clone = new Node(this.name,this.age,this.id);
+			return clone;
+		}
+		
+		
+
+	}
